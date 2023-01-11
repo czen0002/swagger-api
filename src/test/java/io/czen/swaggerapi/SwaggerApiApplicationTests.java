5febc1f21b4c85be9fd439206cbb812801e5f7ae
czen0002 <zc114789@gmail.com>
2023-01-11 16:42:06 +1100

    feat: init git & add all files

---
+++ src/test/java/io/czen/swaggerapi/SwaggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.czen.swaggerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
